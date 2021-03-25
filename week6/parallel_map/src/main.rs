2cf75bcbd80b7a2c612527e2bee849525e713682
murray501 <m68286832@gmail.com>
2021-03-25 23:07:39 +0900

    parallel_map

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,42 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    let (sender, receiver) = crossbeam_channel::unbounded();
+    let (sender2, receiver2) = crossbeam_channel::unbounded();
+    
+    let mut threads = Vec::new();
+
+    for _ in 0..num_threads {
+        let receiver = receiver.clone();
+        let sender2 = sender2.clone();
+        threads.push(thread::spawn(move || {
+            while let Ok(next_num) = receiver.recv() {
+                let output = f(next_num);
+                sender2
+                    .send(output)
+                    .expect("Tried writing to channel, but there are no receivers!");
+                
+            }
+        }))
+    }
+
+    for elem in input_vec {
+        sender
+            .send(elem)
+            .expect("Tried writing to channel, but there are no receivers!");
+    }
+
+    drop(sender);
+
+    for thread in threads {
+        thread.join().expect("Panic occurred in thread");
+    }
+
+    for _ in 0..output_vec.capacity() {
+        let output = receiver2.recv().expect("should receive output");
+        output_vec.push(output);
+    }
+
     output_vec
 }
 
